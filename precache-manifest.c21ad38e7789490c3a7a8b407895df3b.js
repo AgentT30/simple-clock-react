1e2b3828f72d4af1955c2abfe06954a55e22065e
AgentT30 <cthekkunja304@gmail.com>
2020-08-20 22:12:36 +0530

    Updates

---
+++ precache-manifest.c21ad38e7789490c3a7a8b407895df3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6091f22301196231b252e5ad7222810a",
+    "url": "/simple-clock-react/index.html"
+  },
+  {
+    "revision": "276dbbe98427abad3688",
+    "url": "/simple-clock-react/static/css/main.49475b34.chunk.css"
+  },
+  {
+    "revision": "850f3dbb6980411e1203",
+    "url": "/simple-clock-react/static/js/2.6fd0898a.chunk.js"
+  },
+  {
+    "revision": "0cce3cdd5176cd8ebcd9219cf9aa461c",
+    "url": "/simple-clock-react/static/js/2.6fd0898a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "276dbbe98427abad3688",
+    "url": "/simple-clock-react/static/js/main.ca89011f.chunk.js"
+  },
+  {
+    "revision": "09e09c0aef4af351299c",
+    "url": "/simple-clock-react/static/js/runtime-main.aa07df94.js"
+  },
+  {
+    "revision": "67cc652ccbd5a7e7614c2884356f57e1",
+    "url": "/simple-clock-react/static/media/Light_Deer.67cc652c.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-clock-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
