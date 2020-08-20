8956b7ddba813dbf0ca8883b3c2ce53de3d64306
AgentT30 <cthekkunja304@gmail.com>
2020-08-20 22:07:33 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Clock from "react-live-clock";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Clock</h1>
+            <div class="clock">
+                <Clock format={"hh:mm:ss"} ticking={true} timezone={"UTC+0530"} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
